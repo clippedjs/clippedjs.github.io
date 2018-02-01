dc827a64d30d34a2a403d3ddbca4736651b51475
inizio <digit4free@gmail.com>
2018-02-01 10:11:40 +0800

    chores: Google analytics ID

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -9,7 +9,7 @@ module.exports = {
   siteDescription: "Make configuration great again", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  googleAnalyticsID: "UA-82450300-1 ", // GA tracking ID.
+  googleAnalyticsID: "UA-108887459-2", // GA tracking ID.
   disqusShortname: "https-ericwindmill-github-io-gatsby-advanced-starter", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
   userName: "User", // Username to display in the author segment.
